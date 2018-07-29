9b74f2c2403fb22e18b92b8ba560c3af8740a739
Manish Dixit <dixitm@outlook.com>
2018-07-30 01:07:44 +0530

    cousera ml(andrew ng) week 3 assignment completion

--- coursera/machineLearning_AndrewNg/machine-learning-ex2/machine-learning-ex2/ex2/costFunctionReg.m
+++ coursera/machineLearning_AndrewNg/machine-learning-ex2/machine-learning-ex2/ex2/costFunctionReg.m
@@ -10,16 +10,22 @@ m = length(y); % number of training examples
 % You need to return the following variables correctly 
 J = 0;
 grad = zeros(size(theta));
-
+[m, n] = size(X);
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
 %               You should set J to the cost.
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+reg_cost_mult=ones(1,n)
+reg_cost_mult(1)=0
 
+J=((ones(1, m)*((-1.*y).*log(sigmoid(([theta]' * [X]')')) - (1.-y).*log(1.-(sigmoid(([theta]' * [X]')')))))/m) + ((lambda/(2*m))*(reg_cost_mult*(theta.^2)))
 
+reg_grad_add=((lambda/m)*theta)
+reg_grad_add(1)=0
 
+grad=((((( ones(1,2) *([sigmoid(([theta]' * [X]')'), -1*y]') ) * X )')/m)) + reg_grad_add
 
 
 % =============================================================
